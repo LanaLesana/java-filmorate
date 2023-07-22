a687b1b89ad86195eb2fb530d259b228985a7613
Olesia <olesia04@yandex.ru>
2023-07-22 15:18:53 +0300

    final ТЗ9-2nd-check

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,12 +4,21 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 public class Film {
+    @NotBlank @NotEmpty @NotNull
     private Integer id;
+    @NotBlank @NotEmpty @NotNull
     private String name;
+    @NotBlank @NotEmpty @NotNull
     private String description;
+    @NotBlank @NotEmpty @NotNull
     private LocalDate releaseDate;
+    @NotBlank @NotEmpty @NotNull
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,16 +3,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 
 public class User {
+    @NotBlank @NotEmpty @NotNull
     private Integer id;
-    @Email
+    @Email @NotBlank @NotEmpty @NotNull
     private String email;
+    @NotBlank @NotEmpty @NotNull
     private String login;
     private String name;
+    @NotBlank @NotEmpty @NotNull
     private LocalDate birthday;
 }
